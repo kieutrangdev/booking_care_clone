c400794d2551c66ae86560efb8edfe713abd34f0
Le Kieu Trang <104716412+kieutrangdev@users.noreply.github.com>
2023-11-30 17:20:08 +0700

    feat-connect-mysql

--- be/src/server.js
+++ be/src/server.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser"
 import configViewEngine from "./config/configViewEngine.js"
 import initWebRoutes from "./route/web.js"
 import dotenv from "dotenv"
+import connectDB from "./config/connectDB.js"
 dotenv.config();
 let app = express()
 
@@ -12,6 +13,8 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 configViewEngine(app)
 initWebRoutes(app)
+
+connectDB()
 
 let port = process.env.PORT || 8000
 app.listen(port, () => {
